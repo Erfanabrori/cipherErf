f6a286d013c5d32e86afdd3a32b4239d9f23d295
Erfanabrori <erfanabrori@gmail.com>
2026-03-10 15:08:30 +0700

    e

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+
+<h1>Dashboard</h1>
+
+<p>Selamat datang di halaman dashboard</p>
+<p>erfan abrori</p>
+
+<ul>
+    
+</ul>
+
+<br>
+
+<a href="/login">Logout</a>
+
+</body>
+</html>
